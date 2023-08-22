07526a11c34dd676a9de7f435416fccbf14d84d4
nishant shekhar <nishant.ranjanasaxena@gmail.com>
2023-08-22 14:16:05 +0530

    Fixing e2e

--- test/e2e/tests/test_resolver_rule.py
+++ test/e2e/tests/test_resolver_rule.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-"""Integration tests for the Route53 ResolverEndpoint resource
+"""Integration tests for the Route53 ResolverRule resource
 """
 
 import boto3
