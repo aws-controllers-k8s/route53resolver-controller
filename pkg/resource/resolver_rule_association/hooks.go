ccec6fa224c47077dfdb68ba01278b99610e73d8
Mai Waly <mai.saleh@siemens.com>
2026-06-02 12:53:42 +0300

    Remove redundant requeue hooks for ResolverRuleAssociation
    The sdk_create_post_set_output and sdk_read_one_post_set_output hooks
    manually requeue when Status != COMPLETE, but this is already handled
    by the synced:when configuration in generator.yaml which generates
    the IsSynced() method in the runtime.
    
    Addresses reviewer feedback in PR #65.

--- pkg/resource/resolver_rule_association/hooks.go
+++ pkg/resource/resolver_rule_association/hooks.go
@@ -1,13 +1,1 @@
 package resolver_rule_association
-
-import (
-	"errors"
-	"time"
-
-	ackrequeue "github.com/aws-controllers-k8s/runtime/pkg/requeue"
-)
-
-var requeueWhileCreating = ackrequeue.NeededAfter(
-	errors.New("association is not COMPLETE yet"),
-	5*time.Second,
-)
--- templates/hooks/resolver_rule_association/sdk_create_post_set_output.go.tpl
+++
@@ -1,3 +0,0 @@
-	if ko.Status.Status != nil && *ko.Status.Status != "COMPLETE" {
-		return &resource{ko}, requeueWhileCreating
-	}
